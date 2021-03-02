f9dd89c63f7297ad3ccfb2a02e4328f0d634242c
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2021-03-02 23:21:52 +0200

    fix api type

--- src/api/serviceApi.ts
+++ src/api/serviceApi.ts
@@ -1,21 +1,21 @@
-import { API_URL } from './constants';
 import axios from 'axios';
+import { API_URL } from './constants';
+
 
 const serviceApi = {
+  async create(data: any) {
+    try {
+      const result = await axios.post(`${API_URL}/api/service/`, data);
 
-	async create(data) {
-		try {
-			const result = await axios.post(`${API_URL}/api/service/`, data);
+      return result.data;
+    } catch (err) {
+      if (err.response && err.response.data) {
+        throw err.response.data;
+      }
 
-			return result.data;
-		} catch (err) {
-			if (err.response && err.response.data) {
-				throw err.response.data;
-			}
-
-			throw err;
-		}
-	}
+      throw err;
+    }
+  },
 };
 
 export default serviceApi;
